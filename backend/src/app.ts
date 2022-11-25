8d5367f85bc7a84f9d0161d721a52d1906e50ce6
vitalii-minchuk <mcmin4doutsch@gmail.com>
2022-11-25 12:17:23 +0200

    basic settings

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,16 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
+import * as dotenv from "dotenv";
 
-// mongoose
-//   .connect()
-//   .then(() => console.log("Server Connected to db"))
-//   .catch(() => console.log("Server Connected to db Error"));
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL as string)
+  .then(() => console.log("Server Connected to db"))
+  .catch(() => console.log("Server Connected to db Error"));
 
 const app: express.Application = express();
-const port: number = 4004;
+const port = process.env.PORT;
 
 app.get("/healthcheck", (req: Request, res: Response) => {
   res.send("OK");
---
+++ frontend/src/components/main-map/constants.ts
@@ -0,0 +1,8 @@
+import { LatLngExpression } from 'leaflet';
+
+const initialMapCenter: LatLngExpression = {
+  lat: 51.505,
+  lng: -0.09,
+};
+
+export default initialMapCenter;
