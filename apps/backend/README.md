63d751ca5dd798b24df00e950f83c3f1e8de999f
vitalii-minchuk <mcmin4doutsch@gmail.com>
2022-12-13 13:17:21 +0200

    front & deploy on render

--- apps/backend/README.md
+++ apps/backend/README.md
@@ -1,1 +1,11 @@
-df
+- git clone https://github.com/vitalii-minchuk/task-comments-be.git
+
+- cd task-comments-be
+
+- docker compose up -d
+
+- npm run docker:db:migrate
+
+for testing:
+
+https://task-comments.netlify.app/
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello");
+import express, { Request, Response } from 'express';
+
+const app: express.Application = express();
+
+const port = 4004;
+
+app.get('/healthcheck', (req: Request, res: Response) => {
+  res.send('OK');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}/`);
+});
